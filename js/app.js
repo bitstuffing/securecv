7a079f0100811f5ec34118afddd75334d4becb2e
Bit <bitstuffing@gmail.com>
2021-12-16 20:06:24 +0100

    fix and update to 2021, added support for url sharing

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ function readTextFile(file){ //ajax without jquery for internet explorer issues
         if(rawFile.readyState === 4){
             if(rawFile.status === 200 || rawFile.status == 0){
                 var allText = rawFile.responseText;
-                alert(allText);
+                //console.log(allText);
             }
         }
     }
@@ -40,6 +40,9 @@ function buildFromJsonContent(content){
         break;
       case 'portfolio':
         buildPorfolio(v);
+        break;
+      case 'current':
+        buildCurrent(v);
         break;
       case 'resume':
         buildResume(v)
@@ -89,6 +92,13 @@ function buildResume(map){
   $("#ulSectionNav").append(sectionNav);
 }
 
+function buildCurrent(map){
+  var current = buildMap(map);
+  $("#mainTag").append(current);
+  var sectionNav = buildSectionNav('current',false);
+  $("#ulSectionNav").append(sectionNav);
+}
+
 function buildBlog(map){
   var blog = buildMap(map);
   $("#mainTag").append(blog);
@@ -115,8 +125,11 @@ function buildSectionNav(name,active){
     case 'portfolio':
       section +='<a href="#portfolio"><i class="fa fa-laptop"></i>Portfolio</a>';
       break;
+    case 'current':
+      section +='<a href="#current"><i class="fa fa-laptop"></i>Actualmente</a>';
+      break;
     case 'resume':
-      section +='<a href="#resume"><i class="glyphicon glyphicon-leaf"></i>Resumen</a>';
+      section +='<a href="#resume"><i class="glyphicon glyphicon-leaf"></i>Pasado</a>';
       break;
     case 'blog':
       section +='<a href="#blog"><i class="fa fa-newspaper-o"></i>Blog</a>';
@@ -145,7 +158,7 @@ function buildMap(map){
       case "class":
         built.addClass(map[key]);
         break;
-      case "stype":
+      case "style":
         built.css(map[key]);
         break;
       case "text":
@@ -161,6 +174,124 @@ function buildMap(map){
   return built;
 }
 
+function utf8_to_b64( str ) {
+    return window.btoa(unescape(encodeURIComponent( str )));
+}
+
+function b64_to_utf8( str ) {
+    return decodeURIComponent(escape(window.atob( str )));
+}
+
+function worker(p){
+  $.get("securecv.enc",function(content){
+    console.log("decoding...");
+    var decrypted = "";
+    if(p instanceof Object){
+      var password = $("#password").val();
+      try{
+        decrypted = crypto.decryptMessage(content,password);
+      }catch(e){
+        $("<div>Ups! Bad password.</div>").dialog({
+          buttons: {
+              "Ok": function()  {
+                  $(this).dialog("close");
+                  window.location.href="";
+              }
+          }
+        });
+      }
+    }else if(p && p.length>0){
+      try{
+        //var password = b64_to_utf8(p);
+        decrypted = crypto.decryptMessage(content,p);
+      }catch(e){
+        $("<div>Ups! Bad password!</div>").dialog({
+          buttons: {
+              "Ok": function()  {
+                  $(this).dialog("close");
+                  window.location.href="";
+              }
+          }
+        });
+      }
+    }else{
+      $("<div>Ups! No password</div>").dialog({
+          buttons: {
+              "Ok": function()  {
+                  $(this).dialog("close");
+                  window.location.href="";
+              }
+          }
+        });
+    }
+
+    console.log("done, perfect!");
+    var jsonContent = $.parseJSON(decrypted);
+    buildFromJsonContent(jsonContent);
+
+    var parentHeight = $(".home").parent().css("height");
+    $(".home").css("height",parentHeight);
+
+    //events TODO configure
+    $(".bit-links a,.scrollLink").click(function(e){
+      e.preventDefault();
+      $(".bit-links li").removeClass("active");
+      $(this).parent().addClass("active");
+      var target = e.target.href;
+      target = target.substring(target.indexOf("#")+1);
+
+      $(".menu-toggle").trigger("click");
+      $('html, body').animate({
+        scrollTop: $("section > ."+target).offset().top -25
+      }, 800, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        //window.location.hash = "#"+target;
+        $("section > ."+target).scrollTop(50);
+      });
+    });
+
+    $('.portfolio-sorting li').on('click', function (e) {
+      e.preventDefault();
+      $(this).closest('li').addClass('active').siblings().removeClass('active');
+      var seclector = $(this).attr('data-filter');
+      $('.grid').isotope({
+          filter: seclector
+      });
+      return false;
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        lazyLoad:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+  });
+  $(window).on("resize",function(){
+    //apply fix for home section
+    var parentHeight = $(".home").parent().css("height");
+    $(".home").css("height",parseInt(parentHeight)-25);
+  });
+
+  if($("#dialog").length>0 && $("#dialog").is(':visible') && $("#dialog").dialog('isOpen')){
+    $("#dialog").dialog("close");
+  }
+  $(".loading").fadeOut("slow");
+//});
+
+}
 
 $(document).ready(function(){
 
@@ -168,84 +299,36 @@ $(document).ready(function(){
       $("body").toggleClass('mobile-menu-active');
   });
 
-  $("#passwordButton").click(function(){
-
-    $.get("securecv.enc",function(content){
-      console.log("decoding...");
-      var password = $("#password").val();
-      var decrypted = crypto.decryptMessage(content,password);
-      console.log("done, perfect!");
-      var jsonContent = $.parseJSON(decrypted);
-      buildFromJsonContent(jsonContent);
-
-      var parentHeight = $(".home").parent().css("height");
-      $(".home").css("height",parentHeight);
-
-      //events TODO configure
-      $(".bit-links a,.scrollLink").click(function(e){
-        e.preventDefault();
-        $(".bit-links li").removeClass("active");
-        $(this).parent().addClass("active");
-        var target = e.target.href;
-        target = target.substring(target.indexOf("#")+1);
-
-        $(".menu-toggle").trigger("click");
-        $('html, body').animate({
-          scrollTop: $("section > ."+target).offset().top -25
-        }, 800, function(){
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          //window.location.hash = "#"+target;
-          $("section > ."+target).scrollTop(50);
-        });
-      });
-
-      $('.portfolio-sorting li').on('click', function (e) {
-        e.preventDefault();
-        $(this).closest('li').addClass('active').siblings().removeClass('active');
-        var seclector = $(this).attr('data-filter');
-        $('.grid').isotope({
-            filter: seclector
-        });
-        return false;
-      });
-
-      $('.owl-carousel').owlCarousel({
-          loop:true,
-          margin:10,
-          nav:true,
-          lazyLoad:true,
-          responsive:{
-              0:{
-                  items:1
-              },
-              600:{
-                  items:3
-              },
-              1000:{
-                  items:5
+  let searchParams = new URLSearchParams(window.location.search);
+  if(searchParams.has('p')){
+    let p = searchParams.get('p');
+    try{
+        var decoded = b64_to_utf8(p);
+        worker(decoded);
+    }catch(e){
+      $("<div>Ups! Bad url</div>").dialog({
+          buttons: {
+              "Ok": function()  {
+                  $(this).dialog("close");
+                  window.location.href="";
               }
           }
-      });
+        });
+    }
+  }else{
+    $("#passwordButton").click(worker);
 
+    $("#dialog").dialog({
+      autoOpen : false,
+      modal : true,
+      show : "blind",
+      hide : "blind"
+    }).on('dialogclose', function(event) {
+       $(".loading").fadeOut("slow");
     });
-    $(window).on("resize",function(){
-      //apply fix for home section
-      var parentHeight = $(".home").parent().css("height");
-      $(".home").css("height",parseInt(parentHeight)-25);
-    });
-    $("#dialog").dialog("close");
-    $(".loading").fadeOut("slow");
-  });
 
-  $("#dialog").dialog({
-    autoOpen : false,
-    modal : true,
-    show : "blind",
-    hide : "blind"
-  }).on('dialogclose', function(event) {
-     $(".loading").fadeOut("slow");
-  });
 
-  $("#dialog").dialog("open");
+    $("#dialog").dialog("open");
+  }
 
 });
--- js/editor.js
+++ js/editor.js
@@ -29,12 +29,29 @@ function decode(){
   var password = $("#password").val();
   var content = $("#encryptedTextArea").val();
   var decrypted = crypto.decryptMessage(content,password);
-  $("#decryptedTextArea").html(decrypted);
+  save("securecv.dec",decrypted);
+  //$("#decryptedTextArea").html(decrypted);
 }
 
 function encode(){
   var password = $("#password").val();
   var content = $("#decryptedTextArea").val();
   var encrypted = crypto.encryptMessage(content,password);
-  $("#encryptedTextArea").html(encrypted);
+  save("securecv.enc",encrypted);
+  //$("#encryptedTextArea").html(encrypted);
+}
+
+function save(filename, data) {
+    const blob = new Blob([data], {type: 'text/csv'});
+    if(window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else{
+        const elem = window.document.createElement('a');
+        elem.href = window.URL.createObjectURL(blob);
+        elem.download = filename;
+        document.body.appendChild(elem);
+        elem.click();
+        document.body.removeChild(elem);
+    }
 }
