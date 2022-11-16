9edc897c6bec24a7190a38b7f0acfc5abcd0ff93
bitstuffing <bit.stuffing@gmail.com>
2022-11-16 11:26:38 +0100

    (WIP) 3.0 evolution with ionic & react

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
-# SecureCV 2.0
-## Publishing your Curriculum vitae in a secure way
+# SecureCV 3.0
+## Publishing your Curriculum Vitae in a secure way
+
+This is the unfinished 3.0 development version, a port of SecureCV 2.0 to deploy modern frontend applications based on frameworks like ReactJS.
 
 ## Why?
 Some people are being forced to use new technologies and show his skills to all world, and this situation forces people to not have privacy.
 
 SecureCV is a little project with a partial solution about this issue without big technological implications.
 
-Now in 2.0 rev. SecureCV was designed to publish online your CV thinking on share a web safely, no crawler or unauthorized information will be automatically ingested to online databases without your known because all is encrypted.
+Before in 2.x and previews revisions, SecureCV was designed to publish online your CV thinking on share a web safely, and demostrate my skills. The real intention is avoid crawlers or unauthorized access to the information. Today this kind of information will NOT be automatically ingested to online databases without your known because all is encrypted.
 
 ## Advantages
 
-There are some advantages about use SecureCV in his 2.0 rev:
+There are some advantages about use SecureCV in his 2.0 rev, but also in 3.0:
 
 - No server is needed
 - Responsive, portable and with modern design.
@@ -22,7 +24,8 @@ There are some advantages about use SecureCV in his 2.0 rev:
 ## Features
 
 - Full compatible with github pages.
-- Simple editor is attached.
+- Simple editor is attached (since 1.0 version).
+- Mobile native application
 
 ## How it works?
 
@@ -33,6 +36,19 @@ You could use the editor to see his guts and how it's working, but basically, co
 This solution was designed in the past by the author @bitstuffing to get an anticrawler site, and this view-based-solution algorithm  was implemented on SecureCV.
 
 ## License
-SecureCV in all versions is coded by @bitstuffing and licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/)
+SecureCV in all versions is coded by @bitstuffing and licensed under [Creative Commons 4.0](https://creativecommons.org/licenses/by/4.0/). The same situation happens with the information (also encrypted one), so in an summary you're not authorized to use that information and the application in commercial environments without explicit authorization of the author.
 
 ##### Powered by [![N|CryptoJS](https://www.gitbook.com/cdn-cgi/image/width=40,height=40,fit=contain,dpr=1,format=auto/https%3A%2F%2Fcryptojs.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-28427.appspot.com%2Fo%2Fspaces%252F-LVOh1OI8lhWfR_994H2%252Favatar.png%3Fgeneration%3D1546620224707409%26alt%3Dmedia)](https://cryptojs.gitbook.io/docs/)
+
+## Dev notes
+
+I will try to dev a tree.js effect to demostrate some skills.
+
+### libraries
+
+npm install three --save
+npm install @types/three --save
+npm install --save @types/jquery
+npm install jquery --save
+npm install crypto-js --save
+npm install buffer
---
+++ src/components/DecryptorContainer.tsx
@@ -0,0 +1,128 @@
+import './DecryptorContainer.css';
+
+import React, { useState, useEffect }  from 'react';
+import { IonLoading, IonContent } from '@ionic/react';
+import { useIonAlert , useIonViewWillEnter, useIonViewDidEnter, useIonViewWillLeave } from '@ionic/react';
+
+import * as $ from "jquery";
+import { logger } from 'workbox-core/_private';
+import { RouteComponentProps } from 'react-router';
+
+import { Buffer } from 'buffer'
+
+var AES = require("crypto-js/aes");
+var CryptoJS = require("crypto-js");
+
+interface ContainerProps extends RouteComponentProps<{
+  p: string;
+}> {}
+
+const DecryptorContainer: React.FC<ContainerProps> = ({ match }) => {
+
+  const [presentAlert] = useIonAlert();
+  
+  const [showLoading, setShowLoading] = useState(false);
+  let isWorking = false;
+  const [showAlert, setShowAlert] = useState(false);
+  const [unencryptedContent, setUnencryptedContent] = useState('Always share your personal information in a secure way');
+  const [dialogHeader, setDialogHeader] = useState('Password required!');
+
+  //RouteComponentProps issues from rect-router, so needs a callback from useEffect
+  useEffect(() => {
+    if(!isWorking){
+      if (match.params.p){
+        isWorking = true;
+        let password = match.params.p;
+        password = Buffer.from(password,'base64').toString('utf-8');
+        unEncrypt(password);
+      }else{
+        setShowLoading(true);
+      }
+    }
+  }, []);
+
+  // when you activate RouteComponentProps for some unknown reason this event function doesn't work
+  useIonViewDidEnter(() => {
+    setShowLoading(true);
+  });
+
+  return (
+    <IonContent>
+      <div id="mainContainer" className="pre-container">
+        <div id="childMainContainer">{unencryptedContent}</div>
+      </div>
+      <IonLoading
+        cssClass="loading"
+        isOpen={showLoading}
+        onDidDismiss={
+          () => {
+            
+            setShowLoading(false);
+            
+            var a = presentAlert({
+              header: 'SecureCV 3.0',
+              subHeader: dialogHeader,
+              //dismiss
+              backdropDismiss : false,
+              onWillDismiss : () => {
+                //logger.debug(a);
+              },
+              onDidDismiss : () => {
+                //setShowLoading(false);
+              },
+              inputs: [
+                { placeholder : "Please enter the secret password", type: "password", name: "password"}
+              ],
+              
+              buttons: [
+                { 
+                  text : 'Decode',
+                  handler: (alertData) => {
+                    setShowAlert(false);
+                    setShowLoading(false);
+                    setUnencryptedContent(alertData.password);
+                    unEncrypt(alertData.password);
+                    return true;
+                  }
+                }
+              ]
+            });
+          }
+        }
+        message={'Please wait...'}
+        duration={500}
+      />
+    </IonContent>
+  );
+
+function unEncrypt(password: string, alertWrongPassword = true){
+  $.get("/cv.enc",function(data){
+    var temp = document.createElement("div");
+    temp.setAttribute("id","app-container");
+    try{ //unencrypt part
+      var decryptedBytes = AES.decrypt(data, password);
+      data = decryptedBytes.toString(CryptoJS.enc.Utf8);
+      if(data.length>0){
+        temp.innerHTML = data;
+        document.getElementById("childMainContainer")?.replaceWith(temp);
+        document.getElementById('mainContainer')?.setAttribute("class","");
+      }else{
+        if(alertWrongPassword){
+          setDialogHeader("Wrong password, try again");
+        }
+        setShowLoading(true);
+      }
+    }catch(err){
+      if(alertWrongPassword){
+        setDialogHeader("Wrong password, try again");
+      }
+      setShowLoading(true);
+    }
+    logger.debug("finished!");
+    isWorking = false;
+  });
+}
+
+};
+
+export default DecryptorContainer;
